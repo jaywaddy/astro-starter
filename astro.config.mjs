7cb6acbcea2504455777ea7baa58c17bd1a5f4b5
Jay Waddy <40751612+jaywaddy@users.noreply.github.com>
2026-07-30 16:15:57 -0700

    Add Astro font API

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,20 @@
 // @ts-check
 import tailwindcss from "@tailwindcss/vite";
-import { defineConfig } from "astro/config";
+import { defineConfig, fontProviders } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
 	vite: {
 		plugins: [tailwindcss()],
 	},
+	fonts: [
+		{
+			provider: fontProviders.fontsource(),
+			name: "Inter",
+			cssVariable: "--font-inter",
+			fallbacks: ["sans-serif"],
+			weights: ["100 900"],
+			styles: ["italic", "normal"],
+		},
+	],
 });
